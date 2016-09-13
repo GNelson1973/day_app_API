5d43bd0bd2f41714e1093c82469f0fa4a3d99cbe
Jorge Oosthoek <jorge.oosthoek@gmail.com>
2016-09-13 16:31:25 +0200

    seed and model fix

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,9 +3,5 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-  has_many :posts
-
-  def handle
-    self.email.split('@')[0]
-  end
+  has_many :days, dependent: :destroy
 end
