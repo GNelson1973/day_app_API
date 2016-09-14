7b0fe00f4143d92c0b76f2a0d263ce179088867f
Jorge Oosthoek <jorge.oosthoek@gmail.com>
2016-09-14 21:17:56 +0200

    add date including date seed

--- app/controllers/days_controller.rb
+++ app/controllers/days_controller.rb
@@ -69,6 +69,6 @@ private
 
   # Never trust parameters from the scary internet, only allow the white list through.
   def day_params
-    params.require(:day).permit(:title, :body, :rating, :image)
+    params.require(:day).permit(:title, :body, :rating, :image, :date)
   end
 end
